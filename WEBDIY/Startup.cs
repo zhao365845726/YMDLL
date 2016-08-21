2f945c6a146ad25c25c4b6cef203f43b9470d70e
MartyZane <zhao365845726@163.com>
2016-08-21 17:01:48 +0800

    加入WEB自定义建设的项目

---
+++ WEBDIY/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WEBDIY.Startup))]
+namespace WEBDIY
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
