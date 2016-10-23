6acd4ac8b21f3f516f64d47b2d02c0406e80e755
MartyZane <zhao365845726@163.com>
2016-10-23 11:21:28 +0800

    删除不必要的文件

---
+++ PilotData_Hzly/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PilotData_Hzly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
