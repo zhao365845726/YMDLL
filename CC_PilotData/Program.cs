15559bccca6b2c94cd87ecd72c64c5e143b179ca
zhao365845726 <MartyZane@hotmail.com>
2017-03-14 22:02:52 +0800

    增加Core项目

---
+++ CC_PilotData/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CC_PilotData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
