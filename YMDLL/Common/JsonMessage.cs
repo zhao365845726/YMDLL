25e559b129e81be16c62bfe4266fc2f9a5533850
MartyZane <zhao365845726@163.com>
2017-08-06 18:25:23 +0800

    1.增加JsonMessage类

---
+++ YMDLL/Common/JsonMessage.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace YMDLL.Common
+{
+    /// <summary>
+    /// 返回消息
+    /// </summary>
+    public class JsonMessage
+    {
+        /// <summary>
+        /// 是否成功
+        /// </summary>
+        public bool Success { get; set; }
+        /// <summary>
+        /// 结果编码
+        /// </summary>
+        public string Code { get; set; }
+        /// <summary>
+        /// 结果消息
+        /// </summary>
+        public string Message { get; set; }
+        /// <summary>
+        /// 备注信息
+        /// </summary>
+        public string Remark { get; set; }
+
+        public override string ToString()
+        {
+            return JsonHelper.ToJson(this);
+        }
+    }
+
+    /// <summary>
+    /// 连连支付返回json格式
+    /// </summary>
+    public class LLPayJsonMessage
+    {
+        /// <summary>
+        /// 交易结果代码
+        /// </summary>
+        public string ret_code { get; set; }
+        /// <summary>
+        /// 交易结果描述
+        /// </summary>
+        public string ret_msg { get; set; }
+
+        public override string ToString()
+        {
+            return JsonHelper.ToJson(this);
+        }
+    }
+
+    /// <summary>
+    /// 返回带模型的Json信息
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class JsonMessage<T>
+    {
+        /// <summary>
+        /// 是否成功
+        /// </summary>
+        public bool Success { get; set; }
+        /// <summary>
+        /// 结果编码
+        /// </summary>
+        public string Code { get; set; }
+        /// <summary>
+        /// 结果消息
+        /// </summary>
+        public string Message { get; set; }
+        /// <summary>
+        /// 返回的数据模型
+        /// </summary>
+        public T Data { get; set; }
+
+        public override string ToString()
+        {
+            return JsonHelper.ToJson(this);
+        }
+    }
+
+    /// <summary>
+    /// 返回带模型的Json信息
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class JsonListMessage<T>
+    {
+        /// <summary>
+        /// 是否成功
+        /// </summary>
+        public bool Success { get; set; }
+        /// <summary>
+        /// 结果编码
+        /// </summary>
+        public string Code { get; set; }
+        /// <summary>
+        /// 结果消息
+        /// </summary>
+        public string Message { get; set; }
+        /// <summary>
+        /// 返回对象模型
+        /// </summary>
+        public Object Obj { get; set; }
+        /// <summary>
+        /// 返回的数据模型
+        /// </summary>
+        public List<T> Data { get; set; }
+        
+        public override string ToString()
+        {
+            return JsonHelper.ToJson(this);
+        }
+    }
+
+    /// <summary>
+    /// 返回Json信息
+    /// </summary>
+    public class JsonListMessage
+    {
+        /// <summary>
+        /// 是否成功
+        /// </summary>
+        public bool Success { get; set; }
+        /// <summary>
+        /// 结果编码
+        /// </summary>
+        public string Code { get; set; }
+        /// <summary>
+        /// 结果消息
+        /// </summary>
+        public string Message { get; set; }
+        /// <summary>
+        /// 返回的数据模型
+        /// </summary>
+        public string Data { get; set; }
+
+        public override string ToString()
+        {
+            return JsonHelper.ToJson(this);
+        }
+    }
+
+    /// <summary>
+    /// 返回带对象的Json信息
+    /// </summary>
+    public class JsonObjectMessage
+    {
+        /// <summary>
+        /// 是否成功
+        /// </summary>
+        public bool Success { get; set; }
+        /// <summary>
+        /// 结果编码
+        /// </summary>
+        public string Code { get; set; }
+        /// <summary>
+        /// 结果消息
+        /// </summary>
+        public string Message { get; set; }
+        /// <summary>
+        /// 返回的数据模型
+        /// </summary>
+        public object Data { get; set; }
+
+        public override string ToString()
+        {
+            return JsonHelper.ToJson(this);
+        }
+    }
+}
