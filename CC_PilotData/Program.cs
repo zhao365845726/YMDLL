f0307cc2ed4b607b4cad9647fa497eacf0811868
MartyZane <zhao365845726@163.com>
2017-11-07 00:26:41 +0800

    M

--- CC_PilotData/Program.cs
+++ CC_PilotData/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using YMDLL.Class;
 
 namespace CC_PilotData
 {
@@ -6,6 +8,12 @@ namespace CC_PilotData
     {
         static void Main(string[] args)
         {
+            CS_OperaWeb ow = new CS_OperaWeb();
+            string strResult = ow.GetWebCode("https://www.caipiaokong.com/chart/bjklb/21.html", Encoding.UTF8);
+
+
+
+
             Console.WriteLine("Hello World!");
         }
     }
