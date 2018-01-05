ae4912a8922b4e8e85f09e3b5f3abfef3807f9ad
zhao365845726 <zhao365845726@163.com>
2018-01-05 22:23:29 +0800

    完善代理IP地址

--- YMDLL/Class/CS_OperaWeb.cs
+++ YMDLL/Class/CS_OperaWeb.cs
@@ -11,7 +11,7 @@ using System.Web.SessionState;
 using System.Data;
 using System.Net;
 using YMDLL.Interface;
-
+using System.Text.RegularExpressions;
 
 namespace YMDLL.Class
 {
@@ -145,6 +145,72 @@ namespace YMDLL.Class
             return m_ClientIPAddress;
         }
 
+        /// <summary> 
+        /// 取得客户端真实IP。如果有代理则取第一个非内网地址 
+        /// </summary> 
+        public string IPAddress
+        {
+            get
+            {
+                string result = String.Empty;
+                result = HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+                if (result != null && result != String.Empty)
+                {
+                    //可能有代理 
+                    if (result.IndexOf(".") == -1)     //没有“.”肯定是非IPv4格式 
+                        result = null;
+                    else
+                    {
+                        if (result.IndexOf(",") != -1)
+                        {
+                            //有“,”，估计多个代理。取第一个不是内网的IP。 
+                            result = result.Replace(" ", "").Replace("'", "");
+                            string[] temparyip = result.Split(",;".ToCharArray());
+                            for (int i = 0; i < temparyip.Length; i++)
+                            {
+                                if (IsIPAddress(temparyip[i])
+                                    && temparyip[i].Substring(0, 3) != "10."
+                                    && temparyip[i].Substring(0, 7) != "192.168"
+                                    && temparyip[i].Substring(0, 7) != "172.16.")
+                                {
+                                    return temparyip[i];     //找到不是内网的地址 
+                                }
+                            }
+                        }
+                        else if (IsIPAddress(result)) //代理即是IP格式 
+                            return result;
+                        else
+                            result = null;     //代理中的内容 非IP，取IP 
+                    }
+                }
+                string IpAddress = (HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"] != null && HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"] != String.Empty) ? HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"] : HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"];
+
+
+                if (null == result || result == String.Empty)
+                    result = HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"];
+
+                if (result == null || result == String.Empty)
+                    result = HttpContext.Current.Request.UserHostAddress;
+                return result;
+            }
+
+        }
+
+        #region bool IsIPAddress(str1) 判断是否是IP格式 
+        /**//// <summary>
+            /// 判断是否是IP地址格式 0.0.0.0
+            /// </summary>
+            /// <param name="str1">待判断的IP地址</param>
+            /// <returns>true or false</returns>
+        public bool IsIPAddress(string str1)
+        {
+            if (str1 == null || str1 == string.Empty || str1.Length < 7 || str1.Length > 15) return false;
+            string regformat = @"^\d{1,3}[\.]\d{1,3}[\.]\d{1,3}[\.]\d{1,3}$";
+            Regex regex = new Regex(regformat, RegexOptions.IgnoreCase);
+            return regex.IsMatch(str1);
+        }
+        #endregion
+
         /// <summary>
         /// 获取客户端当前访问的URL
         /// </summary>
@@ -199,7 +265,7 @@ namespace YMDLL.Class
         /// <param name="url"></param>
         /// <param name="param"></param>
         /// <returns></returns>
-        public string HttpPostData(string url, string param,string contentType = "application/x-www-form-urlencoded")
+        public string HttpPostData(string url, string param, string contentType = "application/x-www-form-urlencoded")
         {
             var result = string.Empty;
             //注意提交的编码 这边是需要改变的 这边默认的是Default：系统当前编码
@@ -240,7 +306,7 @@ namespace YMDLL.Class
         /// <param name="url"></param>
         /// <param name="param"></param>
         /// <returns></returns>
-        public string HttpPostDataByAuth(string url, string param, string user,string password,string contentType = "application/x-www-form-urlencoded")
+        public string HttpPostDataByAuth(string url, string param, string user, string password, string contentType = "application/x-www-form-urlencoded")
         {
             var result = string.Empty;
             //注意提交的编码 这边是需要改变的 这边默认的是Default：系统当前编码
