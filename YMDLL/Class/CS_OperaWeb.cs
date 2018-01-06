0917119713d9213c1d38cdd6beedd82398c7a4e3
MartyZane <zhao365845726@163.com>
2018-01-06 17:04:41 +0800

    完善支付

--- YMDLL/Class/CS_OperaWeb.cs
+++ YMDLL/Class/CS_OperaWeb.cs
@@ -148,51 +148,48 @@ namespace YMDLL.Class
         /// <summary> 
         /// 取得客户端真实IP。如果有代理则取第一个非内网地址 
         /// </summary> 
-        public string IPAddress
+        public string GetRealIPAddress()
         {
-            get
+            string result = String.Empty;
+            result = HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+            if (result != null && result != String.Empty)
             {
-                string result = String.Empty;
-                result = HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
-                if (result != null && result != String.Empty)
+                //可能有代理 
+                if (result.IndexOf(".") == -1)     //没有“.”肯定是非IPv4格式 
+                    result = null;
+                else
                 {
-                    //可能有代理 
-                    if (result.IndexOf(".") == -1)     //没有“.”肯定是非IPv4格式 
-                        result = null;
-                    else
+                    if (result.IndexOf(",") != -1)
                     {
-                        if (result.IndexOf(",") != -1)
+                        //有“,”，估计多个代理。取第一个不是内网的IP。 
+                        result = result.Replace(" ", "").Replace("'", "");
+                        string[] temparyip = result.Split(",;".ToCharArray());
+                        for (int i = 0; i < temparyip.Length; i++)
                         {
-                            //有“,”，估计多个代理。取第一个不是内网的IP。 
-                            result = result.Replace(" ", "").Replace("'", "");
-                            string[] temparyip = result.Split(",;".ToCharArray());
-                            for (int i = 0; i < temparyip.Length; i++)
+                            if (IsIPAddress(temparyip[i])
+                                && temparyip[i].Substring(0, 3) != "10."
+                                && temparyip[i].Substring(0, 7) != "192.168"
+                                && temparyip[i].Substring(0, 7) != "172.16.")
                             {
-                                if (IsIPAddress(temparyip[i])
-                                    && temparyip[i].Substring(0, 3) != "10."
-                                    && temparyip[i].Substring(0, 7) != "192.168"
-                                    && temparyip[i].Substring(0, 7) != "172.16.")
-                                {
-                                    return temparyip[i];     //找到不是内网的地址 
-                                }
+                                return temparyip[i];     //找到不是内网的地址 
                             }
                         }
-                        else if (IsIPAddress(result)) //代理即是IP格式 
-                            return result;
-                        else
-                            result = null;     //代理中的内容 非IP，取IP 
                     }
+                    else if (IsIPAddress(result)) //代理即是IP格式 
+                        return result;
+                    else
+                        result = null;     //代理中的内容 非IP，取IP 
                 }
-                string IpAddress = (HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"] != null && HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"] != String.Empty) ? HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"] : HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"];
-
-
-                if (null == result || result == String.Empty)
-                    result = HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"];
-
-                if (result == null || result == String.Empty)
-                    result = HttpContext.Current.Request.UserHostAddress;
-                return result;
             }
+            string IpAddress = (HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"] != null && HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"] != String.Empty) ? HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"] : HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"];
+
+
+            if (null == result || result == String.Empty)
+                result = HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"];
+
+            if (result == null || result == String.Empty)
+                result = HttpContext.Current.Request.UserHostAddress;
+            return result;
 
         }
 
